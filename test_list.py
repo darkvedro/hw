b109c70b11b4150dd95135eef68f514dc679a97b
darkvedro <98119522+darkvedro@users.noreply.github.com>
2022-02-20 14:38:00 +0300

    Update test_list.py

--- test_list.py
+++ test_list.py
@@ -19,3 +19,45 @@ class TestList:
     @pytest.mark.parametrize('number_parameters', [3, 4, 5, 6])
     def test_L5(self, number_parameters):
         assert number_parameters >= self.number[0]
+
+class TestDictionary:
+
+    colors = {'белый': '000', 'красный': '111', 'синий': '222'}
+
+    def test_d1(self):
+        assert self.colors['белый'] == '000'
+
+    def test_d2(self):
+        assert len(self.colors.items()) == 3
+
+    def test_d3(self):
+        assert len(self.colors) == 3
+
+
+    def test_d4(self, dict_params):
+        assert dict_params in self.colors.values()
+
+
+    @pytest.mark.parametrize('dict_params2', ['белый', 'синий', 'красный',])
+    def test_dict_5(self, dict_params2):
+        assert dict_params2 in self.colors.keys()
+
+class TestString:
+
+    ttext = """Launching pytest with arguments test_types"""
+
+    def test_S1(self):
+        assert self.ttext.startswith('Launching')
+
+    def test_S2(self):
+        assert 'pytest with arguments' in self.ttext
+
+    def test_S3(self):
+        assert len(self.ttext) <= 100
+
+    def test_S4(self, string_params):
+        assert string_params in self.ttext
+
+    @pytest.mark.parametrize('part_ttext', ['arguments', 'with'])
+    def test_S5(self, part_ttext):
+        assert part_ttext in self.ttext
