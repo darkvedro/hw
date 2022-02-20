bda1b36fec9aa48097b2db8df9a822f90c26a77b
darkvedro <98119522+darkvedro@users.noreply.github.com>
2022-02-20 14:38:25 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,5 +2,14 @@ import pytest
 
 
 @pytest.fixture(params=[3, 4, 5, 6])
-def list_ofparams(request):
+def list_params(request):
     return request.params
+
+@pytest.fixture(params=['000', '111', '222'])
+def dict_params(request):
+    return request.param
+
+
+@pytest.fixture(params=['arguments', 'with'])
+def string_params(request):
+    return request.param
